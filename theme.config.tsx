9772720ea7feaaebba1c8319fc52e3a9ad06c188
kitamuraDev <masato1999824@icloud.com>
2023-02-09 23:43:57 +0900

    :mag: head のメタ情報を追記 #25

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,32 +1,16 @@
 import React from "react";
 import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
+import logo from "./src/components/Logo";
 
 const config: DocsThemeConfig = {
-  project: {
-    link: "https://github.com/kitamuraDev/javasilver-strategy",
-    // todo: icon追加
-  },
+  project: { link: "https://github.com/kitamuraDev/javasilver-strategy" },
   docsRepositoryBase: "https://github.com/kitamuraDev/javasilver-strategy",
+  logo,
 
-  useNextSeoProps: () => {
-    const { route } = useRouter();
-    if (route !== "/") {
-      return { titleTemplate: "%s - JavaSilver Strategy" };
-    }
-
-    return {
-      titleTemplate: "JavaSilver Strategy - Java Silver 試験対策サンプルコード集",
-    };
-  },
-
-  // todo: icon追加
-  logo: <h1 style={{ fontWeight: "bold" }}>JavaSilver Strategy</h1>,
-
-  // todo: 適宜カスタマイズ
   sidebar: {
     toggleButton: true,
-    defaultMenuCollapseLevel: 1
+    defaultMenuCollapseLevel: 1,
   },
 
   editLink: {
@@ -41,21 +25,67 @@ const config: DocsThemeConfig = {
     ),
   },
 
+  useNextSeoProps: () => {
+    const { route } = useRouter();
+    if (route !== "/") {
+      return { titleTemplate: "%s - JavaSilver Strategy" };
+    }
+
+    return {
+      titleTemplate:
+        "JavaSilver Strategy - Java Silver 試験対策サンプルコード集",
+    };
+  },
+
   head: () => {
     const { asPath } = useRouter();
     const { frontMatter } = useConfig();
     return (
       <>
-        {/* todo: url書き換える */}
-        <meta property="og:url" content={`https://my-app.com${asPath}`} />
+        <meta name="description" content="JavaSilver Strategy document site" />
+        <meta name="og:description" content="JavaSilver Strategy document site" />
+        <meta
+          name="og:title"
+          content={
+            frontMatter.title + " - JavaSilver Strategy" ||
+            "JavaSilver Strategy"
+          }
+        />
         <meta
           property="og:title"
-          content={ frontMatter.title + " - JavaSilver Strategy" || "JavaSilver Strategy" }
+          content={
+            frontMatter.title + " - JavaSilver Strategy" ||
+            "JavaSilver Strategy"
+          }
         />
         <meta
           property="og:description"
-          content={ frontMatter.description || "JavaSilver Strategy document site" }
+          content={
+            frontMatter.description || "JavaSilver Strategy document site"
+          }
         />
+        <meta name="og:image" content="/og.jpg" />
+        <meta name="apple-mobile-web-app-title" content="JavaSilver Strategy" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          href="/favicon-dark.png"
+          type="image/png"
+          media="(prefers-color-scheme: dark)"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/og.jpg" />
+        {/* todo: デプロイ後に対応 */}
+        {/* <meta name="twitter:site:domain" content="nextra.site" />
+        <meta name="twitter:url" content="https://nextra.site" />
+        <meta property="og:url" content={`https://my-app.com${asPath}`} /> */}
       </>
     );
   },
